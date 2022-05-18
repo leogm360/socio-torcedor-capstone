a18242318455cbbc7149e79242eb23a0fa9d22ea
leogm360 <leogm360@gmail.com>
2022-05-18 14:32:17 -0300

    chore: implement hooks

---
+++ src/entities/index.ts
@@ -0,0 +1,7 @@
+import { Address } from "./address.entity";
+import { Club } from "./club.entity";
+import { Partnership } from "./partnership.entity";
+import { Reward } from "./reward.entity";
+import { User } from "./user.entity";
+
+export { Address, Club, Partnership, Reward, User };

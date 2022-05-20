ff61e8706b092ffb8e5d9e7ab708d31affcd9368
Rodrigo <rfirmos@gmail.com>
2022-05-20 18:58:01 -0300

    feat: yup validations in clubs, partnerships, rewards

--- src/routes/club.routes.ts
+++ src/routes/club.routes.ts
@@ -4,14 +4,25 @@ import clubDeleteOneController from "../controller/club/deleteOne.controller";
 import clubListController from "../controller/club/list.controller";
 import clubListOneController from "../controller/club/listOne.controller";
 import clubUpdateOneController from "../controller/club/updateOne.controller";
+import { expressYupMiddleware } from "express-yup-middleware";
+import createClubSchema from "../validations/club/createClub.validation";
+import updateClubSchema from "../validations/club/updateClub.validation";
 
 const routes = Router();
 
 const clubRoutes = () => {
-  routes.post("/", clubCreateController);
+  routes.post(
+    "/",
+    expressYupMiddleware({ schemaValidator: createClubSchema }),
+    clubCreateController
+  );
   routes.get("/", clubListController);
   routes.get("/:club_id", clubListOneController);
-  routes.patch("/:club_id", clubUpdateOneController);
+  routes.patch(
+    "/:club_id",
+    expressYupMiddleware({ schemaValidator: updateClubSchema }),
+    clubUpdateOneController
+  );
   routes.delete("/:club_id", clubDeleteOneController);
 
   return routes;
